1fcb72015f3456f75354a4f7ac670dd8201acf7b
Fabricio M. M. <128242885+fabricio-mm@users.noreply.github.com>
2025-12-27 18:21:58 -0300

    Refatorando a lógica do geofencing TimeEntriesController

--- GeoPointAPI/GeoPointAPI/controllers/TimeEntriesController.cs
+++ GeoPointAPI/GeoPointAPI/controllers/TimeEntriesController.cs
@@ -24,9 +24,6 @@ public class TimeEntriesController : ControllerBase
         if (user == null) return NotFound("User not found");
         
         // TODO: Aqui vai entrar a lógica do GEOFENCING 
-        // 2. GEOFENCING: Busca locais permitidos
-        // Regra: Traz locais onde o UserId é NULL (Sede da Empresa) 
-        //        OU onde o UserId é igual ao do usuário (Home Office dele)
         var allowedLocations = await _context.Locations
             .Where(l => l.UserId == null || l.UserId == dto.UserId)
             .ToListAsync();
@@ -41,7 +38,6 @@ public class TimeEntriesController : ControllerBase
 
         foreach (var loc in allowedLocations)
         {
-            // Convertemos decimal para double para fazer a conta matemática
             double distance = GeoCalculator.CalculateDistanceMeters(
                 (double)dto.Latitude, 
                 (double)dto.Longitude, 
@@ -49,27 +45,23 @@ public class TimeEntriesController : ControllerBase
                 (double)loc.Longitude
             );
 
-            // Verifica se está dentro do raio (ex: 100 metros)
+            // Verifica se está dentro do raio
             if (distance <= loc.RadiusMeters)
             {
                 isInsideFence = true;
                 locationName = loc.Name;
-                break; // Se achou um, já pode parar de procurar
+                break;
             }
         }
 
-        // 3. O Veredito
         if (!isInsideFence)
         {
-            // Opcional: Você pode salvar o ponto mesmo assim mas marcar uma flag "ForaDeLocal"
-            // Mas para este exemplo, vamos bloquear:
             return BadRequest(new 
             { 
                 message = "Você está fora do local de trabalho permitido.",
                 your_coords = new { dto.Latitude, dto.Longitude }
             });
         }
-        // Por enquanto aceitamos tudo
 
         var timeEntry = new TimeEntry()
         {

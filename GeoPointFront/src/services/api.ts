95f57f49363daa1c4e0d4b43514c5f8033bff7a9
fabricio-mm <fabriciomartinsmattos9@gmail.com>
2026-04-22 15:51:47 -0300

    atualizando a rota do VITE env para o vercel

--- GeoPointFront/src/services/api.ts
+++ GeoPointFront/src/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:7145';
+const API_BASE_URL =
+    import.meta.env.VITE_API_BASE_URL || 'http://geopoint-api.runasp.net';
 
 // ==================== ENUMS ====================
 
